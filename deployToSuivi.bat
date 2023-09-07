7d4e8ee767a4eb8788257e21935f0e9f3c7e995b
Damien PORTÉ <damien.porte@soprasteria.com>
2023-09-07 15:10:16 +0200

    chore: Configuring the project.

---
+++ deployToSuivi.bat
@@ -0,0 +1,2 @@
+xcopy .\js "C:\Users\dporte\OneDrive - Sopra Steria\- A - SUIVI\Suivi\SCRIPTS" /s /e /i /d
+xcopy .\css "C:\Users\dporte\OneDrive - Sopra Steria\- A - SUIVI\Suivi\.obsidian\snippets" /s /e /i /d
