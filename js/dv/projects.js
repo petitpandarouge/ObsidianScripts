4cd1f72ede35ce9828d48d53e3499d703fd73111
Damien PORTÉ <damien.porte@soprasteria.com>
2023-09-12 15:58:44 +0200

    feat: Now getting the list of project.

---
+++ js/dv/projects.js
@@ -0,0 +1,22 @@
+﻿
+function get() {
+	let pages = _dv.pages('!"TEMPLATES" and #projet');
+	let projects = pages.file
+	return projects.sort((project) => project.name, "desc");
+}
+
+function renderProjects() {
+	let projects = get();
+	if (projects.length) {
+		_dv.list(projects.link);
+	} else {
+		_dv.paragraph("🎉 Nothing to do ! 🎉");
+	}
+}
+
+module.exports = {
+	init: function (dv) {
+		_dv = dv;
+	},
+    renderProjects: renderProjects
+};
