d035682c1c0255ecdea98a0798324466f93353ea
Damien PORTÉ <damien.porte@soprasteria.com>
2023-09-13 00:29:30 +0200

    Testing new rendering for project.

--- js/dv/projects/view.js
+++ js/dv/projects/view.js
@@ -1,69 +1,105 @@
-﻿let {filter} = input;
+﻿let projects = dv.pages('!"TEMPLATES" and #projet');
+let order    = input.order || 'asc';
 
-const rootNode = dv.el("div", "coucou");
+// SORT
+projects = projects.sort( project => {
+    
+	let lastModifiedDate = project.file.mtime.toFormat("yyyy-MM-dd");
+    return `${lastModifiedDate}`;
+    
+}, order);
 
-// INIT
-renderProjects();
+// RENDER
+let html = `<section class="project-cards">`;
 
-// FUNCTIONS
+for (let i = 0; i < projects.length; i++) {
 
-function listErrors(project) {
-	let explaination = "";
-	if (!project.frontmatter.tags) {
-		explaination += "No tags defined &#10;"
-	}
-    if (!project.frontmatter.tags.includes("projet")) {
-        explaination += "No tags 'projet' defined &#10;"
+    const project = projects[i];
+
+    // Jump ahead to get the most relevant date.
+    let now = moment();
+
+    if ( project.status == 'todo' && project.dates && Object.keys(project.dates).length ) {
+        projectTimestamp = Object.keys(project.dates)[0];
+        let projectDate  = moment( projectTimestamp );
+
+        if ( projectDate.format('YYYY MM DD') == now.format('YYYY MM DD') || projectDate.unix() <= now.unix() ) {
+            project.status = 'today';
+        }
     }
-    if (!project.frontmatter.casquette) {
-		explaination += "No casquette defined &#10;"
-	}
-    if (!project.frontmatter.parents) {
-		explaination += "No parents defined &#10;"
-	}
-    if (!project.frontmatter.suivi) {
-		explaination += "No suivi defined &#10;"
-	}
-	if (explaination) {
-		project.containsErrors = true;
-		project.errorExplaination = explaination;
-		return;
-	}
-	project.containsErrors = false;
-	return;
+    
+    html += `<article class="project-card">`;
+        
+    // ICON
+    if ( project.status ) html += `<span class="project-card-status" data-status="${project.status}">&nbsp;</span>`;
+
+    // TITLE
+    let title = project.title || project.file.name;
+    html += `<h1 class="project-card-title"><a href="${project.file.name}" data-href="${project.file.name}" class="internal-link">${title}</a></h1>`;
+
+    // SUBTITLE
+    html += `<div class="project-card-meta">`;
+
+    if ( project.subtitle ) html += `<span class="project-card-subtitle">${project.subtitle}</span>`;
+
+    html += '</div>';
+
+    // DATES
+    if ( project.dates && Object.keys(project.dates).length ) { for (let l = 0; l < Object.keys(project.dates).length; l++) {
+        const itemTimestamp = Object.keys(project.dates)[l];
+        const itemText      = project.dates[ itemTimestamp ];
+
+        let itemDate        = moment( itemTimestamp );
+        let itemHasTime     = ( itemTimestamp.split(' ').length > 1 );
+
+        
+        let sameYear        = ( now.format('YYYY') == itemDate.format('YYYY') );
+
+        let displayDate     = itemDate.calendar(null, {
+            sameDay: '[Today]',
+            nextDay: '[Tomorrow]',
+            nextWeek: 'dddd',
+            lastDay: '[Yesterday]',
+            lastWeek: '[Last] dddd',
+            sameElse: ( sameYear ? 'D MMMM' : 'D MMMM YYYY' ),
+        });
+
+        if ( itemHasTime ) {
+            displayDate += ' <span class="project-card-sep">•</span> ' + itemDate.format( 'h:mm a' );
+        }
+
+        html += `<div class="project-card-date">
+            <span class="project-card-date-prefix" title="${itemDate}">${displayDate}</span>
+            <span class="project-card-date-text" title="${itemDate}">${itemText}</span>
+        </div>`;
+            
+    }}
+
+	// LAST MODIFICATION
+	let dateTime = project.file.mtime.toFormat("dd-MM-yyyy");
+	html += `<div class="project-card-date">
+		<span class="project-card-date-prefix">Dernière modification</span>
+		<span class="project-card-date-text">${dateTime}</span>
+	</div>`;
+
+    // LINKS
+    html += `<div class="project-card-meta">`;
+
+    if ( project.links && project.links.length ) { for (let l = 0; l < project.links.length; l++) {
+        let linkText = project.links[l].split(': ')[0];
+        let linkURL  = project.links[l].split(': ')[1];
+        html += `<a class="project-card-link" href="${linkURL}">${linkText}</a>`;
+            
+    }}
+
+    html += '</div>';
+
+    html += '</div>';
+
+    html += '</article>';
+
 }
 
-function preFormat(project) {
-	listErrors(project);
-	return project;
-}
+html += `</section>`;
 
-function format(project) {
-    console.log(project.name);
-    console.log(project.errorExplaination);
-    return project;
-}
-
-function getBy(filter) {
-	let pages = dv.pages('!"TEMPLATES" and #projet');
-	let projects = pages.file
-        .map(preFormat);
-    if (filter) {
-        projects = projects.filter(filter);
-    }
-    projects = projects.map(format);
-	return projects.sort((project) => project.name, "desc");
-}
-
-function renderProjects() {
-	let projects = getBy(input.filter);
-	if (projects.length) {
-		dv.list(projects.link);
-	} else {
-		dv.paragraph("🎉 Nothing to do ! 🎉");
-	}
-}
-
-function listProjects() {
-
-}
+return html;
