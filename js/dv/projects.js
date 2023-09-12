f7f7529ae35474b6ab26fc9a82a8ec2e4c023289
Damien PORTÉ <damien.porte@soprasteria.com>
2023-09-12 16:30:32 +0200

    feat: First version of getting the projects containing errors.

--- js/dv/projects.js
+++ js/dv/projects.js
@@ -1,12 +1,55 @@
 ﻿
-function get() {
+function listErrors(project) {
+	let explaination = "";
+	if (!project.frontmatter.tags) {
+		explaination += "No tags defined &#10;"
+	}
+    if (!project.frontmatter.tags.includes("projet")) {
+        explaination += "No tags 'projet' defined &#10;"
+    }
+    if (!project.frontmatter.casquette) {
+		explaination += "No casquette defined &#10;"
+	}
+    if (!project.frontmatter.parents) {
+		explaination += "No parents defined &#10;"
+	}
+    if (!project.frontmatter.suivi) {
+		explaination += "No suivi defined &#10;"
+	}
+	if (explaination) {
+		project.containsErrors = true;
+		project.errorExplaination = explaination;
+		return;
+	}
+	project.containsErrors = false;
+	return;
+}
+
+function preFormat(project) {
+	listErrors(project);
+	return project;
+}
+
+function format(project) {
+    console.log(project.name);
+    console.log(project.errorExplaination);
+    return project;
+}
+
+
+function getBy(filter) {
 	let pages = _dv.pages('!"TEMPLATES" and #projet');
 	let projects = pages.file
+        .map(preFormat);
+    if (filter) {
+        projects = projects.filter(filter);
+    }
+    projects = projects.map(format);
 	return projects.sort((project) => project.name, "desc");
 }
 
-function renderProjects() {
-	let projects = get();
+function renderProjects(filter) {
+	let projects = getBy(filter);
 	if (projects.length) {
 		_dv.list(projects.link);
 	} else {
