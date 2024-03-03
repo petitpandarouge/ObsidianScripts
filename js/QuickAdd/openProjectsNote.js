6b76005f5e330962209f60f683f5188aea3dfa34
Damien PORTÉ <damien.porte@soprasteria.com>
2024-03-03 22:52:01 +0100

    feat: Adding a script to open the projects note.

---
+++ js/QuickAdd/openProjectsNote.js
@@ -0,0 +1,15 @@
+﻿module.exports = async (params) => {
+    const projectsFile = app.vault.getAbstractFileByPath("REQUETES/Projets.md");
+    let leaf = app.workspace.getLeafById("dce9100844ff524f");
+    if (!leaf) {
+        leaf = app.workspace.activeLeaf;
+        if (!leaf) {
+            return;
+        }
+    }
+    await leaf.openFile(projectsFile);
+    const view = app.workspace.getActiveViewOfType(params.obsidian.MarkdownView);
+    if (view) {
+        view.editor.focus();
+    }
+}
