39077ecfbc82b93b2b8d636f7813902991987d36
Damien PORTÉ <damien.porte@soprasteria.com>
2024-03-03 22:50:57 +0100

    style: Clean code.

--- js/QuickAdd/setActiveFilePathAsVariable.js
+++ js/QuickAdd/setActiveFilePathAsVariable.js
@@ -4,7 +4,6 @@
 	let activeFile = app.workspace.getActiveFile();
 	if (activeFile) {
 		let i = 1;
-		debugger
 		while (params.variables[`${baseVarName}${i}`] !== undefined) { i++; }
 		params.variables[`${baseVarName}${i}`] = activeFile.path;
 	}
--- js/QuickAdd/setTaskNoteParent.js
+++ js/QuickAdd/setTaskNoteParent.js
@@ -1,5 +1,4 @@
 module.exports = async (params) => {
     const {update} = app.plugins.plugins["metaedit"].api;	
-    debugger
     await update("tache de", "[[" + params.variables["FileBasename1"] + "]]", params.variables["FilePath1"]);
 }
