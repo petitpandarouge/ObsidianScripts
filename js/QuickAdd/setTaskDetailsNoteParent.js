6edd9ad036380d340bf1edefbda5b7e6b7b32827
Damien Porté <>
2024-04-21 12:24:48 +0200

    chore: Deleting the useless scripts.

--- js/QuickAdd/setTaskDetailsNoteParent.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (params) => {
-    const {quickAddApi: {inputPrompt, suggester}} = params;
-    const {update, getPropertiesInFile} = app.plugins.plugins["metaedit"].api;	
-    await update("parents", "[[" + params.variables["ActiveFileBasename"] + "]]", params.variables["NewFileFullname"]);
-}
