155cd23a807c2dac23a3a693ed55b4f460395f68
Damien Porté <>
2024-05-11 00:14:36 +0200

    chore: Computing moduleNameMapper from tsconfig.json

--- ts/jest.config.js
+++ ts/jest.config.js
@@ -1,9 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
+function makeModuleNameMapper(srcPath, tsconfigPath) {
+  const {paths} = require(tsconfigPath).compilerOptions;
+  const aliases = {};
+  Object.keys(paths).forEach((item) => {
+      const key = item.replace('/*', '/(.*)');
+      const path = paths[item][0].replace('/*', '/$1');
+      aliases[key] = srcPath + '/' + path;
+  });
+  return aliases;
+}
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  moduleNameMapper: {
-    '@obsidian/(.*)': '<rootDir>/src/$1'
-  }
+  moduleNameMapper: makeModuleNameMapper('<rootDir>', './tsconfig.json')
 };
