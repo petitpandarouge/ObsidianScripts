0c8eb6f77ac9f8927357ab4e0cebf1ec926b0c44
Damien Porté <>
2024-05-11 00:24:02 +0200

    chore: Remove commented code on generated js.

--- ts/dist/iUrgencyable.js
+++ ts/dist/iUrgencyable.js
@@ -1,7 +1,2 @@
 "use strict";
-// module Obsidian {
-//     export interface IUrgencyable {
-//         getUrgency(): Urgency;
-//     }
-// }
 //# sourceMappingURL=iUrgencyable.js.map
--- ts/dist/urgency.js
+++ ts/dist/urgency.js
@@ -1,15 +1,7 @@
 "use strict";
-// import { IDataview } from "./iDataview";
-// import { ITodoConfiguration } from "./iTodoConfiguration";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Urgency = void 0;
 class Urgency {
-    // #dv: IDataview;
-    // #conf: ITodoConfiguration;
-    // constructor(dv: IDataview, conf: ITodoConfiguration) {
-    //     this.#dv = dv
-    //     this.#conf = conf;
-    // }
     compute() {
     }
     getScore() {
