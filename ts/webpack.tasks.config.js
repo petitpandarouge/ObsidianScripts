2d79090344a5bcea115f86139ba3a6b39de336bd
Damien Porté <>
2024-05-11 01:22:13 +0200

    chore: Using webpack to pack a single js from several ts.

---
+++ ts/webpack.tasks.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/tasks.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'tasks.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
