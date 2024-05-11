6a80ee3171304b173ecc8911f35e3f7f11cb4a59
Damien Porté <>
2024-05-11 19:13:40 +0200

    chore: Using tsconfig-paths-jest plugin to use tsconfig paths in jest.

--- ts/jest.config.js
+++ ts/jest.config.js
@@ -1,19 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
-function makeModuleNameMapper(srcPath, tsconfigPath) {
-  const {paths} = require(tsconfigPath).compilerOptions;
-  const aliases = {};
-  Object.keys(paths).forEach((item) => {
-      const key = item.replace('/*', '/(.*)');
-      const path = paths[item][0].replace('/*', '/$1');
-      aliases[key] = srcPath + '/' + path;
-  });
-  return aliases;
-}
+const tsconfig = require("./tsconfig.json");
+const makeModuleNameMapper = require("tsconfig-paths-jest");
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  moduleNameMapper: makeModuleNameMapper('<rootDir>', './tsconfig.json')
+  moduleNameMapper: makeModuleNameMapper(tsconfig)
 };
