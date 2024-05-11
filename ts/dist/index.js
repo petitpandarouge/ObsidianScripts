22bdf2485d53846c87f6e7b6e2fec9f71e4c2c83
Damien Porté <>
2024-05-11 18:44:35 +0200

    chore: Adding clean script taken in account in build.

--- ts/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const urgency_1 = require("@obsidian/urgency");
-let urgency = new urgency_1.Urgency();
-urgency.getScore();
-//# sourceMappingURL=index.js.map
