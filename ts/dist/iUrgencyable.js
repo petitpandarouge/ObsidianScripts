07ba666584265d6bf500dd943fb1bd824c26cca4
Damien Porté <>
2024-05-11 18:51:23 +0200

    chore: Removing generated files.

--- ts/dist/iUrgencyable.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=iUrgencyable.js.map
--- ts/dist/tasks.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const urgency_1 = require("../src/urgency");
-let urgency = new urgency_1.Urgency();
-urgency.getScore();
-//# sourceMappingURL=tasks.js.map
