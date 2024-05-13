9dc5b9d651a481cfc8223cb4b9ccac5c13838d8a
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-13 21:58:17 +0200

    feat: PluginLoader should add a new command in the loaded plugin

---
+++ ts/src/user-plugins/newProject.ts
@@ -0,0 +1,8 @@
+﻿import { PluginLoader } from '@obsidian/user-plugins/pluginLoader';
+import { Plugin } from '@obsidian/user-plugins/plugin';
+
+// export const loader: PluginLoader = {
+//     onload: async (plugin: Plugin) => {
+//         plugin.addCommand("command");
+//     }
+// };
---
+++ ts/src/user-plugins/pluginLoader.ts
@@ -0,0 +1,14 @@
+﻿import { Plugin } from '@obsidian/user-plugins/plugin';
+import { Command } from '@obsidian/user-plugins/command';
+
+export class PluginLoader {
+    #command: Command;
+    constructor(command: Command) {
+        this.#command = command;
+	}
+
+    onload(plugin: Plugin): Promise<void> {
+        plugin.addCommand(this.#command);
+        return Promise.resolve();
+    }
+}
