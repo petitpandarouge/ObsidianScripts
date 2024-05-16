ba4db1df310d8370a7053c5f84aeb4f90bf5a3bf
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-16 23:58:54 +0200

    feat: newUniqueNoteCommand should call at least once plugin.app.vault.create - green

--- ts/__tests__/user-plugins/newUniqueNoteCommand.spec.ts
+++ ts/__tests__/user-plugins/newUniqueNoteCommand.spec.ts
@@ -1,8 +1,8 @@
 ﻿import { loader as newUniqueNote } from '@obsidian/user-plugins/newUniqueNote';
 import { NewUniqueNoteCommand } from '@obsidian/user-plugins/newUniqueNoteCommand';
 
-describe('newUniqueNote', () => {
-    it('should add a new command in the loaded plugin', async () => {
+describe('newUniqueNoteCommand', () => {
+    it('should call at least once plugin.app.vault.create', async () => {
         // Arrange
         const mockPlugin = {
             addCommand: jest.fn(),
@@ -12,9 +12,10 @@ describe('newUniqueNote', () => {
                 },
             },
         };
+        const newUniqueNoteCommand = new NewUniqueNoteCommand(mockPlugin);
         // Act        
-        await newUniqueNote.onload(mockPlugin);
+        await newUniqueNoteCommand.callback();
         // Assert
-        expect(mockPlugin.addCommand).toHaveBeenCalledWith(expect.any(NewUniqueNoteCommand));
-    }); 
+        expect(mockPlugin.app.vault.create).toHaveBeenCalled();
+    });   
 });
