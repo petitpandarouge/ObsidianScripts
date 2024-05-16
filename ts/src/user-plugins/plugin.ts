453e44af7baa80b4d7072c1994396d9ec74eecbb
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-16 22:33:37 +0200

    feat: newUniqueNote should call at least once plugin.app.vault.create - red

--- ts/src/user-plugins/plugin.ts
+++ ts/src/user-plugins/plugin.ts
@@ -1,5 +1,7 @@
-﻿import { Command } from '@obsidian/user-plugins/command';
+﻿import { App } from '@obsidian/app';
+import { Command } from '@obsidian/user-plugins/command';
 
 export interface Plugin {
     addCommand: (command: Command) => void;
+    app: App;
 }
