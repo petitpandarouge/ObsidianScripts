9c4015540f006195aec2b69b4bda1a1ed6ce92ab
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-17 22:51:04 +0200

    refactor: Going with a command builder.

--- ts/src/user-plugins/abstractCommand.ts
+++ ts/src/user-plugins/abstractCommand.ts
@@ -1,8 +1,6 @@
 ﻿import { Plugin } from '@obsidian/user-plugins/plugin';
 import { Command } from '@obsidian/user-plugins/command';
 
-export type Constructor<TCommand extends AbstractCommand> = new (plugin: Plugin) => TCommand;
-
 export abstract class AbstractCommand implements Command {
     protected plugin: Plugin;
     constructor(plugin: Plugin) {
---
+++ ts/src/user-plugins/abstractPluginLoader.ts
@@ -0,0 +1,10 @@
+﻿import { Plugin } from '@obsidian/user-plugins/plugin';
+import { AbstractCommand } from '@obsidian/user-plugins/abstractCommand';
+
+export abstract class AbstractPluginLoader{
+    abstract buildCommand(plugin: Plugin): AbstractCommand;
+    onload(plugin: Plugin): Promise<void> {
+        plugin.addCommand(this.buildCommand(plugin));
+        return Promise.resolve();
+    }
+}
