5160e97c834411a673542c814581a8a44cd5908c
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 21:58:56 +0200

    feat: ErrorNoticer should notice Obsidian for the time given to the ErrorNoticer if Error is thrown by the inner execution

--- ts/src/errorNoticer.ts
+++ ts/src/errorNoticer.ts
@@ -2,12 +2,13 @@
 import { Notice } from "@obsidian/notice";
 
 export class ErrorNoticer {
+    constructor(private noticeTime: number = 5000) { }
     async wrap(action: Action): Promise<void> {
         try {
             await action();
         } catch (error: unknown) {
             if (error instanceof Error) {
-                new Notice(error.message, 5000);
+                new Notice(error.message, this.noticeTime);
             }
             throw error;
         }
