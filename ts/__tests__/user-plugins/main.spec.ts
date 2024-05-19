47bf5f01e4b37623c3898ebead0a98b35f710228
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 21:46:03 +0200

    feat: main should load the commands into the plugin using the CommandLoader

---
+++ ts/__tests__/user-plugins/main.spec.ts
@@ -0,0 +1,32 @@
+﻿jest.mock('@obsidian/user-plugins/commandLoader', () => {
+    return {
+        CommandLoader: jest.fn(),
+    };
+  });
+
+import { MockPlugin } from '@obsidian/tests/user-plugins/mocks/mockPlugin';
+import { CommandLoader } from '@obsidian/user-plugins/commandLoader';
+import { onload } from '@obsidian/user-plugins/main';
+
+describe('main', () => {
+    it('should load the commands into the plugin using the CommandLoader', async () => {
+        // Arrange
+        const mockPlugin = new MockPlugin();
+        let loadSpy;
+        const mockCommandLoader = jest.fn().mockImplementation(() => {
+            let commandLoader = {
+                load: jest.fn()
+            };
+            loadSpy = jest.spyOn(commandLoader, 'load');
+            return commandLoader;
+        });
+        (CommandLoader as jest.Mock) = mockCommandLoader;
+        // Act
+        await onload(mockPlugin);
+        // Assert
+        expect(mockCommandLoader).toHaveBeenCalledTimes(1);
+        expect(mockCommandLoader).toHaveBeenCalledWith(mockPlugin);
+        expect(loadSpy).toHaveBeenCalledTimes(1);
+        expect(loadSpy).toHaveBeenCalledWith(expect.any(Array));
+    });   
+});
---
+++ ts/src/user-plugins/main.ts
@@ -0,0 +1,7 @@
+﻿import { CommandLoader } from "@obsidian/user-plugins/commandLoader";
+import { Plugin } from "@obsidian/user-plugins/plugin";
+
+export async function onload(plugin: Plugin): Promise<void> {
+    const commandLoader = new CommandLoader(plugin);
+    await commandLoader.load([]);
+}
