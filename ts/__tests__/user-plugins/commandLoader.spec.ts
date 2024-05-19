d1c22c597954ab94de2b7d88b0e6ced53a807d50
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 14:11:31 +0200

    feat: CommandLoader should add commands in the UserPlugins plugin

---
+++ ts/__tests__/user-plugins/commandLoader.spec.ts
@@ -0,0 +1,27 @@
+﻿import Chance from 'chance';
+import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
+import { CommandLoader } from '@obsidian/user-plugins/commandLoader';
+
+describe('CommandLoader', () => {
+    it('should add commands in the UserPlugins plugin', async () => {
+        // Arrange
+        const mockPlugin = {
+            addCommand: jest.fn(),
+            app: {
+                vault: {
+                    create: jest.fn(),
+                },
+            },
+        };
+        const chance = new Chance();
+        const buildersCount = chance.integer({ min: 0, max: 10 });
+        const mockCommandBuilder: CommandBuilder = jest.fn();
+        const builders = Array.from({ length: buildersCount }, () => mockCommandBuilder);
+        const loader = new CommandLoader(mockPlugin);
+        // Act
+        await loader.load(builders);
+        // Assert
+        expect(mockCommandBuilder).toHaveBeenCalledTimes(buildersCount);
+        expect(mockCommandBuilder).toHaveBeenCalledWith(mockPlugin);
+    });   
+});
---
+++ ts/src/user-plugins/commandBuilder.ts
@@ -0,0 +1,4 @@
+﻿import { AbstractCommand } from "@obsidian/user-plugins/abstractCommand";
+import { Plugin } from "@obsidian/user-plugins/plugin";
+
+export type CommandBuilder = (plugin: Plugin) => AbstractCommand;
