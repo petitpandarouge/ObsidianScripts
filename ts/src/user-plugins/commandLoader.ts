ddbaa661d48c7e03a6b3f5c77f95ca6f8cf634a6
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 14:11:31 +0200

    feat: CommandLoader should add commands in the UserPlugins plugin

---
+++ ts/src/user-plugins/commandLoader.ts
@@ -0,0 +1,9 @@
+﻿import { Plugin } from "@obsidian/user-plugins/plugin";
+import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
+
+export class CommandLoader {
+    constructor(private plugin: Plugin) { }
+    async load(builders: CommandBuilder[]): Promise<void> {
+        await Promise.all(builders.map(builder => builder(this.plugin)));
+    }
+}
