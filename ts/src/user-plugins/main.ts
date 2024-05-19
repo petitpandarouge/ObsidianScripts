efa09c75d4034a5e4e12e3aec49b0e8d9f88280c
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 22:04:35 +0200

    feat: First version with the command loader.

--- ts/src/user-plugins/main.ts
+++ ts/src/user-plugins/main.ts
@@ -1,7 +1,16 @@
-﻿import { CommandLoader } from "@obsidian/user-plugins/commandLoader";
+﻿import { UniqueNameGenerator } from "@obsidian/uniqueNameGenerator";
+import { CommandLoader } from "@obsidian/user-plugins/commandLoader";
+import { NewProjectCommand } from "@obsidian/user-plugins/newProjectCommand";
+import { NewUniqueNoteCommand } from "@obsidian/user-plugins/newUniqueNoteCommand";
 import { Plugin } from "@obsidian/user-plugins/plugin";
 
 export async function onload(plugin: Plugin): Promise<void> {
     const commandLoader = new CommandLoader(plugin);
-    await commandLoader.load([]);
+    await commandLoader.load([
+        (plugin) => new NewProjectCommand(plugin),
+        (plugin) => {
+            const uniqueNameGenerator = new UniqueNameGenerator();
+            return new NewUniqueNoteCommand(plugin, uniqueNameGenerator);
+        }
+    ]);
 }
