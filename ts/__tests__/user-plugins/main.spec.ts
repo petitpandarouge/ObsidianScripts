8abd907845c88fa2acbaa200b4b49263f5a96efd
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 23:09:05 +0200

    tests: Finalizing the main code coverage.

--- ts/__tests__/user-plugins/main.spec.ts
+++ ts/__tests__/user-plugins/main.spec.ts
@@ -7,6 +7,8 @@
 import { MockPlugin } from '@obsidian/tests/user-plugins/mocks/mockPlugin';
 import { CommandLoader } from '@obsidian/user-plugins/commandLoader';
 import { onload } from '@obsidian/user-plugins/main';
+import { NewProjectCommand } from '@obsidian/user-plugins/newProjectCommand';
+import { NewUniqueNoteCommand } from '@obsidian/user-plugins/newUniqueNoteCommand';
 
 describe('main', () => {
     it('should load the commands into the plugin using the CommandLoader', async () => {
@@ -28,5 +30,9 @@ describe('main', () => {
         expect(mockCommandLoader).toHaveBeenCalledWith(mockPlugin);
         expect(loadSpy).toHaveBeenCalledTimes(1);
         expect(loadSpy).toHaveBeenCalledWith(expect.any(Array));
+        // Call the functions passed to load and check if they return the correct types.
+        const [newProjectCommandFactory, newUniqueNoteCommandFactory] = loadSpy!.mock.calls[0][0];
+        expect(newProjectCommandFactory(mockPlugin)).toBeInstanceOf(NewProjectCommand);
+        expect(newUniqueNoteCommandFactory(mockPlugin)).toBeInstanceOf(NewUniqueNoteCommand);
     });   
 });
