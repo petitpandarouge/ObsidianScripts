c86003cb0c69ad5326668827e17893338c4e2e4e
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 22:32:25 +0200

    fix: Wrapping the Obsidian Notice class.

---
+++ ts/src/noticeWrapper.ts
@@ -0,0 +1,8 @@
+﻿// The Notice class only exists in Obsidian, so we need to declare it to make it exists in this code. 
+declare const Notice: any;
+
+export class NoticeWrapper {
+    constructor(private message: string, private timeoutInMs: number) { 
+        new Notice(this.message, this.timeoutInMs);
+    }
+}
