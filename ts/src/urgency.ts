291eac8f89566b572ab9c9cab8670094f260f6e8
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 21:56:57 +0200

    refactor: Clean code.

--- ts/src/urgency.ts
+++ ts/src/urgency.ts
@@ -1,15 +1,5 @@
-// import { IDataview } from "./iDataview";
-// import { ITodoConfiguration } from "./iTodoConfiguration";
 
 export class Urgency {
-    // #dv: IDataview;
-    // #conf: ITodoConfiguration;
-
-    // constructor(dv: IDataview, conf: ITodoConfiguration) {
-    //     this.#dv = dv
-    //     this.#conf = conf;
-    // }
-    
     compute() {
 
     }
