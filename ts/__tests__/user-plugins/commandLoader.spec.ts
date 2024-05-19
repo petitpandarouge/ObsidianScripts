ab71c7aa0a7790b8aa4f8fd30232192a23490a63
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 15:49:29 +0200

    feat: CommandLoader should build command

--- ts/__tests__/user-plugins/commandLoader.spec.ts
+++ ts/__tests__/user-plugins/commandLoader.spec.ts
@@ -1,21 +1,33 @@
 ﻿import Chance from 'chance';
 import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
 import { CommandLoader } from '@obsidian/user-plugins/commandLoader';
-import { MockPlugin } from './mocks/mockPlugin';
+import { MockPlugin } from '@obsidian/tests/user-plugins/mocks/mockPlugin';
 
 describe('CommandLoader', () => {
-    it('should add commands in the UserPlugins plugin', async () => {
+    it('should build command', async () => {
         // Arrange
         const mockPlugin = new MockPlugin();
-        const chance = new Chance();
-        const buildersCount = chance.integer({ min: 0, max: 10 });
         const mockCommandBuilder: CommandBuilder = jest.fn();
-        const builders = Array.from({ length: buildersCount }, () => mockCommandBuilder);
+        const builders = [ mockCommandBuilder ];
         const loader = new CommandLoader(mockPlugin);
         // Act
         await loader.load(builders);
         // Assert
-        expect(mockCommandBuilder).toHaveBeenCalledTimes(buildersCount);
+        expect(mockCommandBuilder).toHaveBeenCalledTimes(1);
         expect(mockCommandBuilder).toHaveBeenCalledWith(mockPlugin);
-    });   
+    });  
+    // it('should add commands in the UserPlugins plugin', async () => {
+    //     // Arrange
+    //     const mockPlugin = new MockPlugin();
+    //     const chance = new Chance();
+    //     const buildersCount = chance.integer({ min: 0, max: 10 });
+    //     const mockCommandBuilder: CommandBuilder = jest.fn();
+    //     const builders = Array.from({ length: buildersCount }, () => mockCommandBuilder);
+    //     const loader = new CommandLoader(mockPlugin);
+    //     // Act
+    //     await loader.load(builders);
+    //     // Assert
+    //     expect(mockCommandBuilder).toHaveBeenCalledTimes(buildersCount);
+    //     expect(mockCommandBuilder).toHaveBeenCalledWith(mockPlugin);
+    // });   
 });
