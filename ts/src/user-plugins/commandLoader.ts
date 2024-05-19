363244656e9110d081c49e1c597bd5e50e00a5fc
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 21:45:33 +0200

    feat: Using the command notifier into the command loader.

--- ts/src/user-plugins/commandLoader.ts
+++ ts/src/user-plugins/commandLoader.ts
@@ -1,16 +1,23 @@
 ﻿import { Plugin } from "@obsidian/user-plugins/plugin";
 import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
 import { IdValidator } from "@obsidian/idValidator";
+import { ErrorNoticer } from "@obsidian/errorNoticer";
 
 export class CommandLoader {
-    constructor(private plugin: Plugin) { }
-    load(builders: CommandBuilder[]): Promise<void> {
+    #errorNoticer: ErrorNoticer;
+    constructor(private plugin: Plugin) { 
+        this.#errorNoticer = new ErrorNoticer();
+    }
+    async load(builders: CommandBuilder[]): Promise<void> {
+        await this.#errorNoticer.wrap(async () => await this.#innerLoad(builders));
+    } 
+    async #innerLoad(builders: CommandBuilder[]): Promise<void> {
         const unicityValidator = new IdValidator<string>();
         for (const builder of builders) {
             const command = builder(this.plugin);
             unicityValidator.validate(command, (id) => `UserPlugins : Command with id ${id} already exists.`);
             this.plugin.addCommand(command);
         }
-        return Promise.resolve();
+        await Promise.resolve();
     }
 }
