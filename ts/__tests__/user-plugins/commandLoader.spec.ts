6cf8319d81249e386f2829466d806d9cf6c48cac
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 15:04:48 +0200

    refactor: Creating a default mock plugin.

--- ts/__tests__/user-plugins/commandLoader.spec.ts
+++ ts/__tests__/user-plugins/commandLoader.spec.ts
@@ -1,18 +1,21 @@
-﻿import Chance from 'chance';
+﻿// jest.mock('./mocks/mockPlugin', () => {
+//     return jest.fn().mockImplementation(() => {
+//         return new class extends MockPlugin{
+//             // Mock methods and properties here
+//             addCommand = jest.fn((command) => command.id = 'mocked');
+//           };
+//     });
+//   });
+
+import Chance from 'chance';
 import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
 import { CommandLoader } from '@obsidian/user-plugins/commandLoader';
+import { MockPlugin } from './mocks/mockPlugin';
 
 describe('CommandLoader', () => {
     it('should add commands in the UserPlugins plugin', async () => {
         // Arrange
-        const mockPlugin = {
-            addCommand: jest.fn(),
-            app: {
-                vault: {
-                    create: jest.fn(),
-                },
-            },
-        };
+        const mockPlugin = new MockPlugin();
         const chance = new Chance();
         const buildersCount = chance.integer({ min: 0, max: 10 });
         const mockCommandBuilder: CommandBuilder = jest.fn();
---
+++ ts/__tests__/user-plugins/mocks/mockPlugin.ts
@@ -0,0 +1,10 @@
+﻿import { Plugin } from "@obsidian/user-plugins/plugin";
+
+export class MockPlugin implements Plugin {
+    addCommand = jest.fn();
+    app = {
+        vault: {
+            create: jest.fn(),
+        },
+    };
+}
