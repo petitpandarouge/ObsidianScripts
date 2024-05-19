628bfb161b8f44afb1a037a992485cc33f3855f0
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 15:57:14 +0200

    feat: CommandLoader should build as many commands as provided builders

--- ts/src/user-plugins/commandLoader.ts
+++ ts/src/user-plugins/commandLoader.ts
@@ -4,9 +4,7 @@ import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
 export class CommandLoader {
     constructor(private plugin: Plugin) { }
     async load(builders: CommandBuilder[]): Promise<void> {
-        builders[0](this.plugin);
-        return Promise.resolve();
         // await Promise.all(builders.map(builder => this.plugin.addCommand(builder(this.plugin))));
-        //await Promise.all(builders.map(builder => builder(this.plugin)));
+        await Promise.all(builders.map(builder => builder(this.plugin)));
     }
 }
