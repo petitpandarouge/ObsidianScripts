341e92a2fc6f2c303a966a40ef71475cc062a868
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 21:02:44 +0200

    test: To be seen with Guy.

--- ts/__tests__/errorNoticer.spec.ts
+++ ts/__tests__/errorNoticer.spec.ts
@@ -8,8 +8,8 @@ import { ErrorNoticer } from '@obsidian/errorNoticer';
 import { Notice } from '@obsidian/notice';
 import Chance from 'chance';
 
-describe('ErrorNotifier', () => {
-    it('should notice Obsidian and rethrow if exception is thrown by the inner execution', async () => {
+describe('ErrorNoticer', () => {
+    it('should notice Obsidian and rethrow if Error is thrown by the inner execution', async () => {
         // Arrange
         const chance = new Chance();
         const errorMessage = chance.string();
@@ -35,7 +35,34 @@ describe('ErrorNotifier', () => {
         // Assert
         expect(mockNotice).toHaveBeenCalledTimes(0);
     });
+    // TODO - See this with Guy
+    // it('should notice Obsidian if an unknown error is thrown by the inner execution', async () => {
+    //     // Arrange
+    //     const chance = new Chance();
+    //     const errorNoticer = new ErrorNoticer();
+    //     const wrappedAction = jest.fn(() => { throw new UnknownError(chance.string()) });
+    //     const mockNotice = jest.fn();
+    //     (Notice as jest.Mock) = mockNotice;
+    //     const errorMessage = `An unknown error occured in ${wrappedAction.name} occurred. Update the plugin to fix it.`;
+    //     // Act
+    //     const action = async () => await errorNoticer.wrap(wrappedAction);
+    //     // Assert
+    //     await expect(action).rejects.toThrow();
+    //     expect(mockNotice).toHaveBeenCalledTimes(1);
+    //     expect(mockNotice).toHaveBeenCalledWith(errorMessage, expect.any(Number));
+    // });
 });
+
+
+// class UnknownError {
+//     constructor(public message: string) {}
+// }
+// class UnknownError extends Error {
+//   constructor(public message: string) {
+//       super(message);
+//       this.name = 'UnknownError';
+//   }
+// }
 
 // TODO : Timeout
 // TODO : Rethrow
