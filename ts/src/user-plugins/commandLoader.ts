a1ae95b39e9db4d31b3923acfbd796fb18dedbb3
Damien PORTÉ <damien.porte@soprasteria.com>
2024-05-19 17:19:17 +0200

    feat: CommandLoader should throw if at least two commands have the same id

--- ts/src/user-plugins/commandLoader.ts
+++ ts/src/user-plugins/commandLoader.ts
@@ -1,9 +1,19 @@
 ﻿import { Plugin } from "@obsidian/user-plugins/plugin";
 import { CommandBuilder } from "@obsidian/user-plugins/commandBuilder";
 
+// TODO - Implement an id verifier
 export class CommandLoader {
     constructor(private plugin: Plugin) { }
-    async load(builders: CommandBuilder[]): Promise<void> {
-        await Promise.all(builders.map(builder => this.plugin.addCommand(builder(this.plugin))));
+    load(builders: CommandBuilder[]): Promise<void> {
+        const ids = new Set();
+        for (const builder of builders) {
+            const command = builder(this.plugin);
+            if (ids.has(command.id)) {
+                throw new Error(`Duplicate command id: ${command.id}`);
+            }
+            ids.add(command.id);
+            this.plugin.addCommand(command);
+        }
+        return Promise.resolve();
     }
 }
