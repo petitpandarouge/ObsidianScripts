b12243b5d1cbf2ec3a6854d745a4071d3e9d7c34
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-24 14:46:32 +0200

    chore: Let the error raise on dataview view bundle generation

--- obsinflate/dv.view.build.js
+++ obsinflate/dv.view.build.js
@@ -1,16 +1,12 @@
 const fs = require('fs');
 
-try {
-    const viewName = process.argv[2];
-    const entryPoint = `
-    let view = new obsinflate_dv_view_${viewName.toLowerCase()}.${viewName}();
-    view.build(dv, input);`;
-    const encoding = 'utf-8';
-    fs.appendFileSync(
-        `./bundles/dv/${viewName.toLowerCase()}/view.js`,
-        entryPoint,
-        encoding
-    );
-} catch (err) {
-    console.log('Error appending data to file in sync mode', err);
-}
+const viewName = process.argv[2];
+const entryPoint = `
+let view = new obsinflate_dv_view_${viewName.toLowerCase()}.${viewName}();
+view.build(dv, input);`;
+const encoding = 'utf-8';
+fs.appendFileSync(
+    `./bundles/dv/${viewName.toLowerCase()}/view.js`,
+    entryPoint,
+    encoding
+);
