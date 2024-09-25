41517945385f87a170b1a75821752faa76e9ec59
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-25 23:21:11 +0200

    feat: #19 - Implementing noticer sample in the user plugins hello world.

--- obsinflate/src/user-plugins/hello-world/helloWorldCommand.ts
+++ obsinflate/src/user-plugins/hello-world/helloWorldCommand.ts
@@ -1,15 +1,18 @@
 ﻿import { UserPlugins } from '@obsinflate/user-plugins/UserPlugins';
 import { AbstractCommand } from '@obsinflate/abstractCommand';
-import { Notice } from 'obsidian';
+import { Noticer } from '@obsinflate/infrastructure/noticer';
 
 export class HelloWorldCommand extends AbstractCommand<UserPlugins> {
-    constructor(plugin: UserPlugins) {
+    constructor(
+        plugin: UserPlugins,
+        private noticer: Noticer
+    ) {
         super(plugin);
     }
     id: string = 'hello-world';
     name: string = 'Hello World';
     callback(): Promise<void> {
-        new Notice('Hello World !', 5000);
+        this.noticer.notice('Hello World !', 5000);
         return Promise.resolve();
     }
 }
--- obsinflate/src/user-plugins/hello-world/main.ts
+++ obsinflate/src/user-plugins/hello-world/main.ts
@@ -1,8 +1,14 @@
-﻿import { UserPlugins } from '@obsinflate/user-plugins/UserPlugins';
+﻿import { Noticer } from '@obsinflate/infrastructure/noticer';
+import { UserPlugins } from '@obsinflate/user-plugins/UserPlugins';
 import { CommandLoader } from '@obsinflate/user-plugins/commandLoader';
 import { HelloWorldCommand } from '@obsinflate/user-plugins/hello-world/helloWorldCommand';
 
 export async function onload(plugin: UserPlugins): Promise<void> {
     const commandLoader = new CommandLoader(plugin);
-    await commandLoader.load([(plugin) => new HelloWorldCommand(plugin)]);
+    await commandLoader.load([
+        (plugin) => {
+            const noticer = new Noticer();
+            return new HelloWorldCommand(plugin, noticer);
+        }
+    ]);
 }
