676d4718c04f5241ee950e2d5d903ed566d498ef
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-25 22:56:51 +0200

    feat: #19 - Implementing a noticer sample in the Dataview hello world

--- obsinflate/src/dataview/hello-world/main.ts
+++ obsinflate/src/dataview/hello-world/main.ts
@@ -1,21 +1,25 @@
 import { DataviewApi } from 'obsidian-dataview';
 import { ViewBuilder } from '@obsinflate/dataview/viewBuilder';
 import { View } from '@obsinflate/dataview/view';
+import { Noticer } from '@obsinflate/infrastructure/noticer';
 
 class Input {
     name!: string;
 }
 
 class HelloWorld implements View {
+    constructor(private noticer: Noticer) {}
     render(dv: DataviewApi, input: Input) {
         const header = input.name ? `Hello ${input.name}` : 'Hello World';
         dv.header(1, header);
         dv.table(['Name', 'Ids'], [['Me', 0]]);
+        this.noticer.notice('View built !', 5000);
     }
 }
 
 export class HelloWorldBuilder implements ViewBuilder {
     build() {
-        return new HelloWorld();
+        const noticer = new Noticer();
+        return new HelloWorld(noticer);
     }
 }
