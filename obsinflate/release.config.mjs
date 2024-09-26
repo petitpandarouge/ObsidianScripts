205e76f5a4361ad21c212aa32f56a8ee406dd73f
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-27 00:37:05 +0200

    feat: Adding the changelog in the release build by semantic release.

--- obsinflate/release.config.mjs
+++ obsinflate/release.config.mjs
@@ -7,6 +7,7 @@ export default {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         '@semantic-release/github',
         [
             '@semantic-release/git',
