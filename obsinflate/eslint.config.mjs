250890c2cb2cb9ebdbf1171b4f9262e3e41485cc
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-26 15:10:59 +0200

    chore: Adding warning raise on TODO comments in the code.

--- obsinflate/eslint.config.mjs
+++ obsinflate/eslint.config.mjs
@@ -11,6 +11,13 @@ export default tseslint.config(
             '@typescript-eslint/no-unused-vars': [
                 'error',
                 { argsIgnorePattern: '^_' }
+            ],
+            'no-warning-comments': [
+                'warn',
+                {
+                    terms: ['todo', 'fixme'],
+                    location: 'start'
+                }
             ]
         }
     }
