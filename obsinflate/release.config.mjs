69ef77ba8b3b251861608b871391509a440c1e2e
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-27 00:02:38 +0200

    feat: Test of the semantic release configuration.

---
+++ obsinflate/release.config.mjs
@@ -0,0 +1,20 @@
+﻿/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ['main'],
+    tagFormat: 'obsinflate-${version}',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ]
+    ]
+};
