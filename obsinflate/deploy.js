be9ac5fb25a62a3eb9c762f858ef9dd1a987b627
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-26 18:39:18 +0200

    feat: #23 - Script to deploy in prod vault.

---
+++ obsinflate/deploy.js
@@ -0,0 +1,12 @@
+﻿const { execSync } = require('child_process');
+const { config } = require('./package.json');
+
+try {
+    execSync(`npm run bundle && ncp ./bundles ${config.obsidian.vaults.prod}`, {
+        stdio: 'inherit'
+    });
+    console.log(`Deployment to "${config.obsidian.vaults.prod}" successful!`);
+} catch (error) {
+    console.error('Deployment failed:', error);
+    process.exit(1);
+}
