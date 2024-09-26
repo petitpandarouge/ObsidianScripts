9af24becdca27564cc9307bd7f42388b426cca4c
Damien PORTÉ <damien.porte@soprasteria.com>
2024-09-26 13:16:19 +0200

    tests: #22 - Object that does not implement Error cannot be catched by toThrow().

--- obsinflate/tests/errorNoticer.spec.ts
+++ obsinflate/tests/errorNoticer.spec.ts
@@ -53,32 +53,27 @@ describe('ErrorNoticer', () => {
         // Assert
         expect(mockNoticer.notice).toHaveBeenCalledTimes(0);
     });
-    // TODO - See this with Guy
-    // it('should notice Obsidian if an unknown error is thrown by the inner execution', async () => {
-    //     // Arrange
-    //     const chance = new Chance();
-    //     const errorNoticer = new ErrorNoticer();
-    //     const wrappedAction = jest.fn(() => { throw new UnknownError(chance.string()) });
-    //     const mockNotice = jest.fn();
-    //     (Notice as jest.Mock) = mockNotice;
-    //     const errorMessage = `An unknown error occured in ${wrappedAction.name} occurred. Update the plugin to fix it.`;
-    //     // Act
-    //     const action = async () => await errorNoticer.wrap(wrappedAction);
-    //     // Assert
-    //     await expect(action).rejects.toThrow();
-    //     expect(mockNotice).toHaveBeenCalledTimes(1);
-    //     expect(mockNotice).toHaveBeenCalledWith(errorMessage, expect.any(Number));
-    // });
+    it('should notice Obsidian if an error that is not an instance of Error is thrown by the inner execution', async () => {
+        // Arrange
+        const chance = new Chance();
+        const mockNoticer = mock<INoticer>();
+        const errorNoticer = new ErrorNoticer(mockNoticer);
+        const unknownError = { whateverMessage: chance.string() };
+        const wrappedAction = jest.fn(() => {
+            throw unknownError;
+        });
+        //const errorMessage = `An unknown error occured in ${wrappedAction.name} occurred. Update the plugin to fix it.`;
+        // Act
+        const action = async () => await errorNoticer.wrap(wrappedAction);
+        // Assert
+        // toThrow() only works with Error instances.
+        await expect(action).rejects.toEqual(unknownError);
+        // expect(mockNoticer.notice).toHaveBeenCalledTimes(1);
+        // expect(mockNoticer.notice).toHaveBeenCalledWith(
+        //     errorMessage,
+        //     expect.any(Number)
+        //);
+    });
 });
-
-// class UnknownError {
-//     constructor(public message: string) {}
-// }
-// class UnknownError extends Error {
-//   constructor(public message: string) {
-//       super(message);
-//       this.name = 'UnknownError';
-//   }
-// }
 
 // TODO : Timeout
