2ccff22ab0bfb8f12fea2d03fba4140ae6680899
Damien PORTÉ <damien.porte@soprasteria.com>
2024-10-06 15:28:52 +0200

    test: add tests for the IdValidator

---
+++ obsinflate/tests/idValidator.spec.ts
@@ -0,0 +1,48 @@
+﻿import { Identifiable } from '@obsinflate/core/identifiable';
+import { IdValidator } from '@obsinflate/core/idValidator';
+import Chance from 'chance';
+
+describe('IdValidator', () => {
+    it('should not raise an error if all the identifiers provided in an IdValidator instance are differents', async () => {
+        // Arrange
+        const chance = new Chance();
+        const idValidator = new IdValidator<string>();
+        const identifiablesCount = chance.integer({ min: 1, max: 30 });
+        const mockIdentifiables: Identifiable<string>[] = [];
+        for (let i = 0; i < identifiablesCount; i++) {
+            mockIdentifiables.push({ id: chance.guid() });
+        }
+        // Act
+        const action = () => {
+            for (const identifiable of mockIdentifiables) {
+                idValidator.validate(
+                    identifiable,
+                    (id) =>
+                        `UserPlugins : Command with id ${id} already exists.`
+                );
+            }
+        };
+        // Assert
+        expect(action).not.toThrow();
+    });
+    it('should raise a DuplicatedIdError if two identifiers provided in an IdValidator instance are the same', async () => {
+        // Arrange
+        const chance = new Chance();
+        const idValidator = new IdValidator<string>();
+        const identifiable = { id: chance.guid() };
+        // Act
+        idValidator.validate(
+            identifiable,
+            (id) => `UserPlugins : Command with id ${id} already exists.`
+        );
+        const action = () =>
+            idValidator.validate(
+                identifiable,
+                (id) => `UserPlugins : Command with id ${id} already exists.`
+            );
+        // Assert
+        expect(action).toThrow(
+            `UserPlugins : Command with id ${identifiable.id} already exists.`
+        );
+    });
+});
