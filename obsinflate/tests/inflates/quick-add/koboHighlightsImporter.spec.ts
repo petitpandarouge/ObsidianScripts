6493e963553544ff7526cf5075e7e35cde30e50d
Damien PORTÉ <damien.porte@soprasteria.com>
2024-10-12 19:04:50 +0200

    implementing the begging of the file suggestion

--- obsinflate/tests/inflates/quick-add/koboHighlightsImporter.spec.ts
+++ obsinflate/tests/inflates/quick-add/koboHighlightsImporter.spec.ts
@@ -1,7 +1,26 @@
+import { Parameters } from '@obsinflate/api/quick-add/parameters';
+import { Script } from '@obsinflate/api/quick-add/script';
+import { mockDeep } from 'jest-mock-extended';
+
+class KoboHighlightsImporter implements Script {
+    entry(params: Parameters): Promise<void> {
+        const displayItems: string[] = [];
+        const actualItems: string[] = [];
+        params.quickAddApi.suggester(displayItems, actualItems);
+        return Promise.resolve();
+    }
+}
+
 describe('KoboHighlightsImporter', () => {
-    it.todo(
-        'should suggest the book highlights to import from the "Digital Editions/Annotations" directory ".annot" files'
-    );
+    it('should suggest the book highlights to import from the "Digital Editions/Annotations" directory ".annot" files', async () => {
+        // Arrange
+        const importer = new KoboHighlightsImporter();
+        const mockParams = mockDeep<Parameters>();
+        // Act
+        importer.entry(mockParams);
+        // Assert
+        expect(mockParams.quickAddApi.suggester).toHaveBeenCalledTimes(1);
+    });
     it.todo('should read the content of the selected ".annot" file');
     it.todo('should deserialize the fragment of an annotation');
     it.todo('should deserialize the content of an annotation');
