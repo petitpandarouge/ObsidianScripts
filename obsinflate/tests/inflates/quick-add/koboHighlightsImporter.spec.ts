711b2c62815ac4904eabafc77d2a27abb9c5926d
Damien PORTÉ <damien.porte@soprasteria.com>
2024-10-12 17:47:21 +0200

    writting the specifications as it.todo

---
+++ obsinflate/tests/inflates/quick-add/koboHighlightsImporter.spec.ts
@@ -0,0 +1,18 @@
+describe('KoboHighlightsImporter', () => {
+    it.todo(
+        'should suggest the book highlights to import from the "Digital Editions/Annotations" directory ".annot" files'
+    );
+    it.todo('should read the content of the selected ".annot" file');
+    it.todo('should deserialize the fragment of an annotation');
+    it.todo('should deserialize the content of an annotation');
+    it.todo('should deserialize all the annotations');
+    it.todo('should format the annotations in markdown quotes');
+    it.todo(
+        'should create a markdown file having the name "YYYYMMDDHHmm - Book Title.md"'
+    );
+    it.todo('should apply the "Livre" template to the markdown file');
+    it.todo('should create the author note if it does not already exist');
+    it.todo(
+        'should reference the author note in the "author" property of the book note'
+    );
+});
