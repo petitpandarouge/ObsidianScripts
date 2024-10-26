1910c685ad01892c5aa6504d924e74df34b9d6e5
Damien PORTÉ <damien.porte@soprasteria.com>
2024-10-26 20:27:05 +0200

    implementing epubpoint as string generator

--- obsinflate/tests/data/stringEpubPointGenerator.ts
+++ obsinflate/tests/data/stringEpubPointGenerator.ts
@@ -5,6 +5,11 @@ import {
 import { XHTML_FILE_EXTENSION } from '@obsinflate/core/fileExtensions';
 import Chance from 'chance';
 
+interface IntegerOptions {
+    min: number;
+    max: number;
+}
+
 const EPUB_ELEMENT_INDEXES_TO_OFFSET_SEPARATOR = ':';
 const EPUB_FRAGMENT_IDENTIFIER_PREFIX = '#point(';
 const EPUB_FRAGMENT_IDENTIFIER_SUFFIX = ')';
@@ -12,31 +17,70 @@ const EPUB_FRAGMENT_IDENTIFIER_SUFFIX = ')';
 const chance = new Chance();
 
 export class StringEpubPointGenerator {
-    static generate(): string {
-        const filePath = StringEpubPointGenerator.generateFilePath();
+    static generate(
+        filePathElementsCount: Partial<IntegerOptions> = { min: 1, max: 5 },
+        fragmentIdentifierPathComponentsElementsCount: Partial<IntegerOptions> = {
+            min: 1,
+            max: 5
+        },
+        fragmentIdentifierPathComponentsRange: Partial<IntegerOptions> = {
+            min: 1,
+            max: 100
+        }
+    ): string {
+        const filePath = StringEpubPointGenerator.generateFilePath(
+            filePathElementsCount
+        );
         const fragmentIdentifier =
-            StringEpubPointGenerator.generateFragmentIdentifier();
+            StringEpubPointGenerator.generateFragmentIdentifier(
+                fragmentIdentifierPathComponentsElementsCount,
+                fragmentIdentifierPathComponentsRange
+            );
         return `${filePath}${fragmentIdentifier}`;
     }
 
-    private static generateFilePath(): string {
+    private static generateFilePath(
+        filePathElementsCount: Partial<IntegerOptions>
+    ): string {
         const pathElements = [];
-        for (let i = 0; i < chance.integer({ min: 1, max: 5 }); i++) {
+        for (let i = 0; i < chance.integer(filePathElementsCount); i++) {
             pathElements.push(chance.word());
         }
         return `${EPUB_POINT_FILE_PATH_PREFIX}${pathElements.join(EPUB_POINT_COMPONENTS_SEPARATOR)}${XHTML_FILE_EXTENSION}`;
     }
 
-    private static generateFragmentIdentifier(): string {
-        return `${EPUB_FRAGMENT_IDENTIFIER_PREFIX}${this.generatePathComponents()}${EPUB_FRAGMENT_IDENTIFIER_SUFFIX}`;
+    private static generateFragmentIdentifier(
+        fragmentIdentifierPathComponentsElementsCount: Partial<IntegerOptions>,
+        fragmentIdentifierPathComponentsRange: Partial<IntegerOptions>
+    ): string {
+        return `${EPUB_FRAGMENT_IDENTIFIER_PREFIX}${this.generatePathComponents(fragmentIdentifierPathComponentsElementsCount, fragmentIdentifierPathComponentsRange)}${EPUB_FRAGMENT_IDENTIFIER_SUFFIX}`;
     }
 
-    private static generatePathComponents(): string {
+    private static generatePathComponents(
+        fragmentIdentifierPathComponentsElementsCount: Partial<IntegerOptions>,
+        fragmentIdentifierPathComponentsRange: Partial<IntegerOptions>
+    ): string {
         const elementIndexes = [];
-        for (let i = 0; i < chance.integer({ min: 1, max: 5 }); i++) {
-            elementIndexes.push(chance.integer({ min: 1, max: 100 }));
+        for (
+            let i = 0;
+            i < chance.integer(fragmentIdentifierPathComponentsElementsCount);
+            i++
+        ) {
+            elementIndexes.push(
+                chance.integer(fragmentIdentifierPathComponentsRange)
+            );
         }
-        const offset = chance.integer({ min: 1, max: 100 });
+        const offset = chance.integer(fragmentIdentifierPathComponentsRange);
+        return StringEpubPointGenerator.formatPathComponents(
+            elementIndexes,
+            offset
+        );
+    }
+
+    private static formatPathComponents(
+        elementIndexes: Number[],
+        offset: Number
+    ): string {
         return `${EPUB_POINT_COMPONENTS_SEPARATOR}${elementIndexes.join(EPUB_POINT_COMPONENTS_SEPARATOR)}${EPUB_ELEMENT_INDEXES_TO_OFFSET_SEPARATOR}${offset}`;
     }
 }
