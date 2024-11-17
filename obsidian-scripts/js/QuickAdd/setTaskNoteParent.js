0f3bbc9cc56d6f451163e08ecd417f31976fa57e
Damien PORTÉ <damien.porte@soprasteria.com>
2024-11-17 23:27:02 +0100

    feat: update the task property name

--- obsidian-scripts/js/QuickAdd/setTaskNoteParent.js
+++ obsidian-scripts/js/QuickAdd/setTaskNoteParent.js
@@ -1,4 +1,4 @@
 module.exports = async (params) => {
     const {update} = app.plugins.plugins["metaedit"].api;	
-    await update("tache de", "[[" + params.variables["FileBasename1"] + "]]", params.variables["FilePath1"]);
+    await update("tache-de", "[[" + params.variables["FileBasename1"] + "]]", params.variables["FilePath1"]);
 }
